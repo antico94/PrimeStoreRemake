d97d77b2f2860f15f2060b14feb2902eea74470d
antico94 <meiuandrei@gmail.com>
2023-03-10 17:17:26 +0200

    Matched theme colour

--- prime-store-remake/src/App.js
+++ prime-store-remake/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello mom!</h1>
-    </div>
-  );
+    const [darkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+        const body = document.body;
+        if (darkMode) {
+            body.classList.remove('lightMode');
+        } else {
+            body.classList.add('lightMode');
+        }
+    }, [darkMode]);
+
+    const darkModeToggle = () => {
+        setDarkMode(!darkMode);
+    };
+
+    return (
+        <div className="App">
+            <button onClick={darkModeToggle}>Toggle</button>
+            <h1>Hello mom!</h1>
+        </div>
+    );
 }
 
 export default App;
