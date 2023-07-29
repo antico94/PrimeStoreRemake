be5f6ee4624bc5eca1e93884e8c6868fe9e634ad
AnTiCoD <meiuandrei@gmail.com>
2023-07-29 17:53:20 +0300

    Made it so i'll never lose my data again

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,14 +1,19 @@
 using Backend.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+builder = WebApplication.CreateBuilder(new WebApplicationOptions { EnvironmentName = "Demo" });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.DocumentFilter<EnvironmentSwaggerFilter>();
+});
+
 builder.Services.AddDbContext<PrimeStoreDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddCors(options =>
@@ -20,9 +25,9 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod();
     });
 });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
@@ -34,6 +39,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseCors(corsPolicyBuilder => corsPolicyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
@@ -42,3 +52,20 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public class EnvironmentSwaggerFilter : IDocumentFilter
+{
+    private readonly IConfiguration _configuration;
+
+    public EnvironmentSwaggerFilter(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        var environmentName = _configuration.GetValue<string>("Environment") ?? "Unknown";
+
+        swaggerDoc.Info.Description += $"\nRunning in {environmentName} environment.";
+    }
+}
